52f193073d2da05ef057732c4a7fdc2c8d662578
William Monk <william.monk@skybettingandgaming.com>
2016-04-28 21:03:20 +0100

    Update Icons To Have a Smooth Animation

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 const menubar = require('menubar')
 const electron = require('electron');
 const path = require('path');
+const Menu = require('menu');
 const ipc = electron.ipcMain;
 const mb = menubar({ 
     transparent: true,
-    icon: path.join(__dirname, String(1) + '.png'),
+    icon: path.join(__dirname, 'icons', String(1) + '.png'),
     height: 150,
-    shadow: true
+    shadow: true,
+    'always-on-top': true
 });
 
 const setIcon = num => {
-    mb.tray.setImage(path.join(__dirname, String(num) + '.png'));
+    mb.tray.setImage(path.join(__dirname, 'icons', String(num) + '.png'));
 };
+
+mb.on('after-create-window', function () {
+    mb.window.openDevTools();
+});
 
 ipc.on('pomodoroComplete', function(event, arg) {
     setIcon(1);
@@ -19,18 +25,21 @@ ipc.on('pomodoroComplete', function(event, arg) {
 });
 
 const timeouts = [];
+var animator = null;
 ipc.on('timerBegin', function (e, id, time) {
     const start = Date.now();
-    const quarter = time * 1000 / 4;
-    timeouts.push(setTimeout(() => setIcon(2), quarter));
-    timeouts.push(setTimeout(() => setIcon(3), quarter * 2));
-    timeouts.push(setTimeout(() => setIcon(4), quarter * 3));
+    var anim = 1;
+    animator = setInterval(() => {
+        if (anim > 359) return;
+        setIcon(anim++);
+    }, time * 1000 / 359);
     timeouts.push(setTimeout(function () {
         e.sender.send('timerEnd', id);
     }, time * 1000));
 });
 
 ipc.on('timerCancel', function () {
+    clearInterval(animator);
     setIcon(1);
     timeouts.forEach(t => {
         clearTimeout(t);
